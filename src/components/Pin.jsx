1409ab8defa13e6a74b5b4491817cd96aa560185
Samrat-14 <samratdewdrops0@gmail.com>
2021-12-24 11:28:43 +0530

    fix

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -13,7 +13,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save }}) => {
     const navigate = useNavigate();
     const user = fetchUser();
 
-    const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.googleId))?.length;
+    const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user?.googleId))?.length;
 
     const savePin = (id) => {
         if(!alreadySaved) {
@@ -22,10 +22,10 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save }}) => {
                 .setIfMissing({ save: [] })
                 .insert('after', 'save[-1]', [{
                     _key: uuidv4(),
-                    userId: user.googleId,
+                    userId: user?.googleId,
                     postedBy: {
                         _type: 'postedBy',
-                        _ref: user.googleId
+                        _ref: user?.googleId
                     }
                 }])
                 .commit()
@@ -95,7 +95,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save }}) => {
                                     {destination.length > 10 ? `${destination.slice(0, 10)}...` : destination}
                                 </a>
                             )}
-                            {postedBy?._id === user.googleId && (
+                            {postedBy?._id === user?.googleId && (
                                 <button 
                                     type="button"
                                     onClick={(e) => {
